c32f11ec81b9bc798390cca1cfedfcc62a2cd8d4
AndreyBoiko <75693099+AndreyBoiko@users.noreply.github.com>
2023-01-07 00:42:48 +0200

    New sorter

--- sorter.py
+++ sorter.py
@@ -1,4 +1,4 @@
-
+# python module\Lesson6\HW6\sorter2.py D:\sorter
 import re, sys, shutil, os
 from pathlib import Path
 
@@ -14,59 +14,66 @@ def is_full(rootdir):
         quit()
 
 
-def new_folders():
-    """Створює потрібні папки, якщо вони вже існують, то помилка не виникає"""
+def main_function(some_path, known_formats, unknown_formats):
+    """Створює потрібні папки. Якщо вони вже існують, то помилка не виникає.
+        Викликає основну функцію. Повертає відоомі і невідомі формати"""
     for k in suffix.keys():
         try:
-            os.mkdir(root_str_path + "\\" + k)
+            os.mkdir(os.path.join(some_path, k))
         except FileExistsError:
             continue
+    listdirs(some_path)
+    if not known_formats:
+        known_formats = None
+    if not unknown_formats:
+        unknown_formats = None
+    return f"Known formats: {known_formats}; Unknown formats: {unknown_formats}"
 
 
 def listdirs(rootdir):
     """Основна функція, перебирає вказану папку"""
-    for string_path in rootdir.iterdir():
-        if string_path.is_dir():
+    for list_path in rootdir.iterdir():
+        if list_path.is_dir():
             is_reserved = False
             for k in suffix.keys():
-                if string_path.name == k:
+                if list_path.name == k:
                     is_reserved = True
             if is_reserved:
                 continue
-            listdirs(Path(string_path))
+            listdirs(Path(list_path))
         else:
-            move_and_rename(string_path)
+            move_and_rename(list_path)
     if rootdir != path:
         os.rmdir(rootdir)
 
 
 def normalize(name):
-    """Переводить назви на латинницю, та нормалізує їх"""
-    name = Path(name)
-    CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
-    TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+    """Нормалізує ім'я файлу"""
+    name_obj = Path(name)
+    cyrillic_symbols = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+    translation = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
                 "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
-    CYRILLIC_CODES = []
-    for char in CYRILLIC_SYMBOLS:
-        CYRILLIC_CODES.append(ord(char))
-    TRANS = dict(zip(CYRILLIC_CODES, TRANSLATION))
-    TRANS.update({ord(chr(c).upper()): l.upper() for c, l in TRANS.items()})
-    clean_name = re.sub(name.suffix, '', name.name)
-    new_name = clean_name.translate(TRANS)
+    cyrillic_codes = []
+    for char in cyrillic_symbols:
+        cyrillic_codes.append(ord(char))
+    trans = dict(zip(cyrillic_codes, translation))
+    trans.update({ord(chr(c).upper()): l.upper() for c, l in trans.items()})
+    clean_name = re.sub(name_obj.suffix, '', name_obj.name)
+    new_name = clean_name.translate(trans)
     return re.sub("[^a-zA-Z0-9]", "_", new_name)
 
 
 def move_and_rename(string_path):
-    """Спочатку переміщує файл, а потім змінює його ім'я вже у папці призначення"""
+    """Тепер спочатку переміщує файл, а потім змінює його ім'я вже у папці призначення"""
     string_path_obj = Path(string_path)
     flag = True
     for k, v in suffix.items():
         if v.find(string_path_obj.suffix) != -1:
-            file_formats.add(string_path_obj.suffix[1:])
-            new_location = root_str_path + "\\" + k
+            known_formats.add(string_path_obj.suffix[1:])
+            new_location = os.path.join(root_str_path, k)
             shutil.move(str(string_path_obj), new_location)  # Перемещаем файл по ключу
-            old = new_location + "\\" + string_path_obj.name
-            new = new_location + "\\" + normalize(string_path_obj.name) + string_path_obj.suffix
+            old = os.path.join(new_location, string_path_obj.name)
+            new = os.path.join(new_location, normalize(string_path_obj.name) + string_path_obj.suffix)
             os.rename(old, new)
             if k == "archives":
                 unpack_archive(new)
@@ -74,15 +81,15 @@ def move_and_rename(string_path):
             break
     if flag:
         unknown_formats.add(string_path_obj.suffix[1:])
-        new_location = root_str_path + "\\" + "other"
+        new_location = os.path.join(root_str_path, "other")
         shutil.move(str(string_path_obj), new_location)  # Перемещаем файл в папку other
-        old = new_location + "\\" + string_path_obj.name
-        new = new_location + "\\" + normalize(string_path_obj.name) + string_path_obj.suffix
+        old = os.path.join(new_location, string_path_obj.name)
+        new = os.path.join(new_location, normalize(string_path_obj.name) + string_path_obj.suffix)
         os.rename(old, new) 
 
 
 def unpack_archive(archive_location_string_path):
-    """Розпаковує архів та видаляє його"""
+    """Розпаковує архів у папку з ідентичною назвою і видаляє його"""
     obj_path = Path(archive_location_string_path)
     folder = re.sub(obj_path.suffix, '', archive_location_string_path)
     os.mkdir(folder)
@@ -90,7 +97,9 @@ def unpack_archive(archive_location_string_path):
     os.remove(archive_location_string_path)
 
 
-"""Словник використовуваних розширень"""
+known_formats = set()
+unknown_formats = set()
+
 suffix = {
     "images": '.jpeg .png .jpg .svg',
     "video": '.avi .mp4 .mov .mkv',
@@ -101,20 +110,13 @@ suffix = {
 }
 
 
-"""Основний код програми"""
 if __name__ == "__main__":
-    root_str_path = sys.argv[1]
+    try:
+        root_str_path = sys.argv[1]
+    except IndexError:
+        input("You did not specify a folder path. Press 'Enter' key to quit")
+        quit()
     path = Path(root_str_path)
-    file_formats = set()
-    unknown_formats = set()
     is_full(path)
-    new_folders()
-    listdirs(path)
-    if not file_formats:
-        file_formats = None
-    if not unknown_formats:
-        unknown_formats = None
-    print("Known formats:", file_formats)
-    print("Unknown formats:", unknown_formats)
+    print(main_function(path, known_formats, unknown_formats))
     input("Press 'Enter' key to quit")
-    
